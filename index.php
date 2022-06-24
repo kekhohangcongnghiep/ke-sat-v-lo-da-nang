8e057149baa1b5c0753bfd03ce8c2f9230ad5728
Kệ kho hàng công nghiệp Onetech <108117054+kekhohangcongnghiep@users.noreply.github.com>
2022-06-24 14:20:16 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,9 @@
+<html>
+<title>Kệ sắt v lỗ đa năng Onetech giá rẻ, độ bền cao</title>
+<body>
+<?php
+echo'<h1>Onetech sản xuất và cung cấp kệ sắt v lỗ đa năng giá rẻ từ nhà máy, độ bền trên 15 năm</h1>';
+header("location: https://kekhotrungtai.com/ke-sat-v-lo-da-nang");
+?>
+</body>
+</html>
